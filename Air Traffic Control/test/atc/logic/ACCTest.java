bcb7bb4d7cb614b03be6c6ba617ba2986d415473
joh739 <joh739@gmail.com>
2011-10-01 16:52:33 +0000

    Added helper class for world wind and removed the not working version. Code now stable.

---
+++ Air Traffic Control/test/atc/logic/ACCTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atc.logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author johan
+ */
+public class ACCTest {
+    
+    public ACCTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of GetCTA method, of class ACC.
+     */
+    @Test
+    public void testGetCTA() {
+        System.out.println("GetCTA");
+        ACC instance = new ACC();
+        instance.GetCTA();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ChangeSpeed method, of class ACC.
+     */
+    @Test
+    public void testChangeSpeed() {
+        System.out.println("ChangeSpeed");
+        ACC instance = new ACC();
+        instance.ChangeSpeed();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of ChangeDirection method, of class ACC.
+     */
+    @Test
+    public void testChangeDirection() {
+        System.out.println("ChangeDirection");
+        ACC instance = new ACC();
+        instance.ChangeDirection();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of TakeOff method, of class ACC.
+     */
+    @Test
+    public void testTakeOff() {
+        System.out.println("TakeOff");
+        ACC instance = new ACC();
+        instance.TakeOff();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Landing method, of class ACC.
+     */
+    @Test
+    public void testLanding() {
+        System.out.println("Landing");
+        ACC instance = new ACC();
+        instance.Landing();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
---
+++ Air Traffic Control/test/atc/logic/CTATest.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atc.logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author johan
+ */
+public class CTATest {
+    
+    public CTATest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of GetActiveAirplane method, of class CTA.
+     */
+    @Test
+    public void testGetActiveAirplane() {
+        System.out.println("GetActiveAirplane");
+        CTA instance = new CTA();
+        instance.GetActiveAirplane();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of DetectAirplane method, of class CTA.
+     */
+    @Test
+    public void testDetectAirplane() {
+        System.out.println("DetectAirplane");
+        CTA instance = new CTA();
+        instance.DetectAirplane();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Collision method, of class CTA.
+     */
+    @Test
+    public void testCollision() {
+        System.out.println("Collision");
+        CTA instance = new CTA();
+        instance.Collision();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
