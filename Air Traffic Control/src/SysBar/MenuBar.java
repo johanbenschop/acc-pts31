86022fc50829439dda6d49466ec58a6082566fa6
joh739 <joh739@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-09 21:22:18 +0000

    Most of the SysBar Launcher is done, but not all of it yet(just details). (yellow background is resolved...)

--- Air Traffic Control/src/SysBar/MenuBar.java
+++ Air Traffic Control/src/SysBar/MenuBar.java
@@ -1,53 +1,57 @@
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
+ * 
+ * Sources: 
+ * http://stackoverflow.com/questions/2158/creating-a-custom-button-in-java
+ * 
  */
 package SysBar;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Event;
-import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.LayoutManager;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.awt.Toolkit;
 import javax.swing.JPanel;
-import javax.swing.OverlayLayout;
 
 /**
  *
  * @author Johan Benschop
  */
-public class MenuBar extends JPanel {
+public final class MenuBar extends JPanel {
 
-    private ArrayList<Button> menuItems;
+    public enum Type {
 
-//    public MenuBar(ArrayList<MenuItem> menuItems) {
-//        this.menuItems = menuItems;
-//    }
+        NORMAL, ALERT, NOTIFICATION;
+    }
+
     public MenuBar() {
         //this.setSize(66, 720);
-        setPreferredSize(new Dimension(65, 720));
-        this.setBackground(Color.yellow);
+        setPreferredSize(new Dimension(66, 720));
+        this.setBackground(Color.BLACK);
         //this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        
-        // Testing items, to be removed!
-        this.add(new Button("Start", Color.BLUE, 0, null, null));
-        this.add(new Button("Add Flight", Color.BLUE, 0, null, null));
-        this.add(new Button("Crash Flight", Color.BLUE, 0, null, null));
     }
 
-    public Iterator<Button> getMenuItems() {
-
-        return menuItems.iterator();
+    public Button addItem(Button menuItem) {
+        //menuItem.addActionListener(this);
+        this.add(menuItem);
+        return menuItem;
     }
 
-    public void addItem(Button menuItem) {
-        menuItems.add(menuItem);
+    public Button addItem(String title, Color colour, int weight, String icon, Type type) {
+        Button menuItem = new Button(title, colour, weight, icon, type);
+        //menuItem.addActionListener(this);
+        this.add(menuItem);
+        return menuItem;
     }
 
-    public void addItem(String title, Color colour, int weight, Image icon, Event onClick) {
-        menuItems.add(new Button(title, colour, weight, icon, onClick));
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image image = Toolkit.getDefaultToolkit().getImage("src/SysBar/resources/launcher_back_65.png");
+        for (int i = 0; i < this.getHeight(); i = i + 4) {
+            g.drawImage(image, 0, i, this);
+        }
     }
 }
