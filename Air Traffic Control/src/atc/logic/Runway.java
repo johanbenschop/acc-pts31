3fef2753b505d7f6d5bd7f537ff5d96bc5f77cbc
teysz007 <teysz007@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-09 20:37:55 +0000

    Runway class & test made, also made forms AddFlight & CommandFlight.

--- Air Traffic Control/src/atc/logic/Runway.java
+++ Air Traffic Control/src/atc/logic/Runway.java
@@ -1,16 +1,43 @@
 package atc.logic;
 
+/**
+ *
+ * @author Mateusz
+ */
 public class Runway {
 
-	private int Length;
-	private int Direction;
+    private int Length;
+    private int Direction;
+    private boolean Availability;
 
-	/**
-	 * 
-	 * @return 
-	 */
-	public void ChangeAvailability() {
-		throw new UnsupportedOperationException();
-	}
+    Runway(int length, int direction, boolean availability) {
+        this.Length = length;
+        this.Direction = direction;
+        this.Availability = availability;
+    }
 
+    public int getLength() {
+        return Length;
+    }
+
+    public int getDirection() {
+        return Direction;
+    }
+
+    public boolean getAvailability() {
+        return Availability;
+    }
+
+    /**
+     * 
+     * @return Availability
+     */
+    public boolean ChangeAvailability() {
+        if (Availability) {
+            Availability = false;
+        } else {
+            Availability = true;
+        }
+        return Availability;
+    }
 }
