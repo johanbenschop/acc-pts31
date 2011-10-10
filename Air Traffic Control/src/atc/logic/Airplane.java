d4d48048689783816e3acc3f7c85b74c136abc16
mrpuha <mrpuha@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-10 07:33:42 +0000

    Update Airplane

--- Air Traffic Control/src/atc/logic/Airplane.java
+++ Air Traffic Control/src/atc/logic/Airplane.java
@@ -1,9 +1,11 @@
 package atc.logic;
 
-public class Airplane {
+import java.util.ArrayList;
+
+public class Airplane extends AirplaneFactory {
 
 	private int Direction;
-	private int Speed;
+	private double Speed;
 	private int MaxSpeed;
 	private int MinSpeed;
 	private int Weight;
@@ -15,13 +17,76 @@ public class Airplane {
 	private int MaxFuel;
 	private int CurrentFuel;
 	private int FuelUsage;
+        private double Altitude;
+        private ArrayList<Runway> Runways;
 
+        
+        public Airplane(int MaxSpeed, int MinSpeed, int Weight, String Type, String Manufacturer, 
+                int PlaneHeight, int PlaneWidth, int PlaneLength, int MaxFuel, int FuelUsage, int Direction, double Speed, int CurrentFuel, double Altitude)
+        {
+            super(MaxSpeed, MinSpeed, Weight, Type, Manufacturer, PlaneHeight, PlaneWidth, PlaneLength, MaxFuel, FuelUsage);
+            this.Direction = Direction;
+            this.Speed = Speed;
+            this.CurrentFuel = CurrentFuel;
+            this.Altitude = Altitude;
+        }
+        
 	/**
 	 * 
 	 * @return 
 	 */
-	public void Fly() {
-		throw new UnsupportedOperationException();
+	public void Fly(int direction, int speed, double altitude) {
+		this.Direction = direction;
+                this.Speed = speed;
+                this.Altitude = altitude;
 	}
+        
+        
+        public void SetAimedSpeed(double speed)
+        {
+            this.Speed = speed;
+        }
+        
+        public void SetAimedDirection(int direction)
+        {
+            this.Direction = direction;
+        }
+        
+        public void SetAimedAltitude(double altitude)
+        {
+            this.Altitude = altitude;
+        }
+        
+        public void SetCurrentFuel()
+        {
+            this.CurrentFuel = (this.MaxFuel - this.FuelUsage);
+        }
+        
+
+    public double getAltitude() {
+        return Altitude;
+    }
+
+    public int getCurrentFuel() {
+        return CurrentFuel;
+    }
+
+    public int getDirection() {
+        return Direction;
+    }
+
+    public double getSpeed() {
+        return Speed;
+    }
+
+       
+
+    void Landing(Runway r, int direction) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    void TakeOff(Runway r, int direction, double height, double speed) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 
 }
