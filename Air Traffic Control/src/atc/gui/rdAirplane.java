3d80502a00d7280038300aa97b92647b81030403
teysz007 <teysz007@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-17 10:54:57 +0000

    Changed rdAirplane so it renders an image.

--- Air Traffic Control/src/atc/gui/rdAirplane.java
+++ Air Traffic Control/src/atc/gui/rdAirplane.java
@@ -14,19 +14,21 @@ import gov.nasa.worldwind.render.DrawContext;
 import gov.nasa.worldwind.render.Material;
 import gov.nasa.worldwind.render.Polygon;
 import gov.nasa.worldwind.render.Renderable;
+import gov.nasa.worldwind.render.ShapeAttributes;
+import java.util.ArrayList;
 
 /**
  *
  * @author johan
  */
-public class rdAirplane implements Renderable {
+public class rdAirplane {
 
     Airplane airplane;
-    private final Box box;
     private Polygon pgon;
 
-    public rdAirplane(Airplane airplane) {
-//        BasicShapeAttributes basicShapeAttributes = new BasicShapeAttributes();
+    public rdAirplane(Airplane airplane)
+    {
+        //        BasicShapeAttributes basicShapeAttributes = new BasicShapeAttributes();
 //        basicShapeAttributes.setInteriorMaterial(Material.RED);
 //        basicShapeAttributes.setEnableLighting(true);
 //        basicShapeAttributes.setOutlineMaterial(Material.BLUE);
@@ -37,15 +39,39 @@ public class rdAirplane implements Renderable {
 //        box = new Box(Position.ZERO, 0x64, 0x64, 0x64);
 //        box.setAttributes(basicShapeAttributes);
 //        box.setValue(AVKey.DISPLAY_NAME, airplane.toString());
+        // Create and set an attribute bundle.
+        ShapeAttributes normalAttributes = new BasicShapeAttributes();
+        normalAttributes.setInteriorMaterial(Material.YELLOW);
+        normalAttributes.setOutlineOpacity(0.5);
+        normalAttributes.setInteriorOpacity(0.8);
+        normalAttributes.setOutlineMaterial(Material.GREEN);
+        normalAttributes.setOutlineWidth(2);
+        normalAttributes.setDrawOutline(true);
+        normalAttributes.setDrawInterior(true);
+        normalAttributes.setEnableLighting(true);
+        ShapeAttributes highlightAttributes = new BasicShapeAttributes(normalAttributes);
+        highlightAttributes.setOutlineMaterial(Material.WHITE);
+        highlightAttributes.setOutlineOpacity(1);
 
-    }
-    
+        ArrayList<Position> pathLocations = new ArrayList<Position>();
+        pathLocations.add(Position.fromDegrees(28, -110, 5e4));
+        pathLocations.add(Position.fromDegrees(35, -108, 5e4));
+        pathLocations.add(Position.fromDegrees(35, -111, 5e4));
+        pathLocations.add(Position.fromDegrees(28, -111, 5e4));
+        pathLocations.add(Position.fromDegrees(28, -110, 5e4));
+        pgon = new Polygon(pathLocations);
+        pgon.setValue(AVKey.DISPLAY_NAME, "Hello!");
+        normalAttributes = new BasicShapeAttributes(normalAttributes);
+        normalAttributes.setDrawInterior(true);
+        normalAttributes.setInteriorMaterial(Material.WHITE);
+        normalAttributes.setInteriorOpacity(1);
+        pgon.setAttributes(normalAttributes);
+        pgon.setHighlightAttributes(highlightAttributes);
+        float[] texCoords = new float[]{0, 0, 1, 0, 1, 1, 0, 1, 0, 0};
+        pgon.setTextureImageSource("images/32x32-icon-nasa.png", texCoords, 5);
+    }            
+            
     public Polygon getRender() {
         return pgon;
-    }
-
-    @Override
-    public void render(DrawContext dc) {
-        dc.addOrderedRenderable(box);
     }
 }
