c213f7e5154b8d067ca8aea3d068a312abf9390a
joh739 <joh739@gmail.com>
2011-10-17 10:39:44 +0000

    Fixed (possible) a bug in the rendering of airplanes(or just singular :/)

--- Air Traffic Control/src/atc/gui/rdAirplane.java
+++ Air Traffic Control/src/atc/gui/rdAirplane.java
@@ -12,6 +12,7 @@ import gov.nasa.worldwind.render.BasicShapeAttributes;
 import gov.nasa.worldwind.render.Box;
 import gov.nasa.worldwind.render.DrawContext;
 import gov.nasa.worldwind.render.Material;
+import gov.nasa.worldwind.render.Polygon;
 import gov.nasa.worldwind.render.Renderable;
 
 /**
@@ -22,20 +23,25 @@ public class rdAirplane implements Renderable {
 
     Airplane airplane;
     private final Box box;
+    private Polygon pgon;
 
     public rdAirplane(Airplane airplane) {
-        BasicShapeAttributes basicShapeAttributes = new BasicShapeAttributes();
-        basicShapeAttributes.setInteriorMaterial(Material.RED);
-        basicShapeAttributes.setEnableLighting(true);
-        basicShapeAttributes.setOutlineMaterial(Material.BLUE);
-        basicShapeAttributes.setOutlineWidth(2d);
-        basicShapeAttributes.setDrawInterior(true);
-        basicShapeAttributes.setDrawOutline(false);
-        
-        box = new Box(Position.ZERO, 0x64, 0x64, 0x64);
-        box.setAttributes(basicShapeAttributes);
-        box.setValue(AVKey.DISPLAY_NAME, airplane.toString());
+//        BasicShapeAttributes basicShapeAttributes = new BasicShapeAttributes();
+//        basicShapeAttributes.setInteriorMaterial(Material.RED);
+//        basicShapeAttributes.setEnableLighting(true);
+//        basicShapeAttributes.setOutlineMaterial(Material.BLUE);
+//        basicShapeAttributes.setOutlineWidth(2d);
+//        basicShapeAttributes.setDrawInterior(true);
+//        basicShapeAttributes.setDrawOutline(false);
+//        
+//        box = new Box(Position.ZERO, 0x64, 0x64, 0x64);
+//        box.setAttributes(basicShapeAttributes);
+//        box.setValue(AVKey.DISPLAY_NAME, airplane.toString());
 
+    }
+    
+    public Polygon getRender() {
+        return pgon;
     }
 
     @Override
