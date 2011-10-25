cb1b3e6c63a760da72504d5d92da5ad3fd08624f
jneek <jneek@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-25 13:18:18 +0000

    Aan CTA gewerkt (Sabrina en Jeanique)

--- Air Traffic Control/src/atc/logic/CTA.java
+++ Air Traffic Control/src/atc/logic/CTA.java
@@ -1,39 +1,72 @@
 package atc.logic;
 
+import java.util.*;
+
 public class CTA {
 
-	private GeoLocation location;
-	private double width;
-	private double length;
-        
-        public CTA(GeoLocation location, double width, double length) {
-            this.location = location;
-            this.width = width;
-            this.length = length;            
+    private GeoLocation location;
+    private double width;
+    private double length;
+    private Airplane airplane;
+    private List<Airplane> airplaneList;
+    private Iterator<Airplane> airplaneIterator;
+    private int airplaneNumber = 1;
+
+    public CTA(GeoLocation location, double width, double length) {
+        this.location = location;
+        this.width = width;
+        this.length = length;
+        airplaneIterator = airplaneList.iterator();
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public Airplane GetActiveAirplane(int AirplaneNumber) {
+        for (Airplane a : airplaneList) {
+            // als a.airplaneNumber == AirplaneNumber 
+            return a;
         }
+        return null; // geef airplane terug of niets als er geen is gevodnen 
+    }
 
-	/**
-	 * 
-	 * @return 
-	 */
-	public void GetActiveAirplane() {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @return 
+     */
+    public void DetectAirplane() {
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @return 
-	 */
-	public void DetectAirplane() {
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * 
+     * @return 
+     */
+    public void Collision() {
+        for (Airplane a : airplaneList) {
+            //int xposa = a.xpos;
+            //if(a.Status == INFLIGHT || a.Status == CRASHING)
+            //{
+            //a.posx + 300;
+            //  for (Airplane b : airplaneList) {
+            //      if(a.posx == b.posx){
+            //      set a.status = CRASHED;
+            //  }
+            //}
+        }
+    }
 
-	/**
-	 * 
-	 * @return 
-	 */
-	public void Collision() {
-		throw new UnsupportedOperationException();
-	}
+    public void addAirplane(Airplane a) {
+        //a.airplaneNumber = airplaneNumber;
+        //airplaneNumber ++;
+        airplaneList.add(a);
 
+    }
+
+    public void deleteAirplane(int AirplaneNumber) {
+        for (Airplane a : airplaneList) {
+            // als a.airplaneNumber == AirplaneNumber dan verwijder.. anders niets
+        }
+    }
 }
