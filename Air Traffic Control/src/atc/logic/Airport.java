c21fe3a580df2713004b1c9a2e0f72fc8411b6b3
skillcoil <skillcoil@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-28 13:09:46 +0000

    Airport getters toegevoegd.

--- Air Traffic Control/src/atc/logic/Airport.java
+++ Air Traffic Control/src/atc/logic/Airport.java
@@ -2,32 +2,57 @@ package atc.logic;
 
 public class Airport {
 
-	private int AirportID;
-	private String City;
-	private String Country;
-	private int IATA_FAA;
-	private int ICAO;
-	private String Timezone;
-	private boolean DST;
-        
-        public Airport(int AirportID, String City, String Country, int ITATA_FAA, int ICAO, String Timezone, boolean DST) {
-            this.AirportID = AirportID;
-            this.City = City;
-            this.Country = Country;
-            this.IATA_FAA = ITATA_FAA;
-            this.ICAO = ICAO;
-            this.Timezone = Timezone;
-            this.DST = DST;
-        }
-	/**
-	 * 
-	 * @return 
-	 */
-	public void getRunway() {
-		throw new UnsupportedOperationException();
-	}
-        
-        public int getAirportID(){
-            return AirportID;
-        }
+    private int AirportID;
+    private String City;
+    private String Country;
+    private int IATA_FAA;
+    private int ICAO;
+    private String Timezone;
+    private boolean DST;
+
+    public Airport(int AirportID, String City, String Country, int ITATA_FAA, int ICAO, String Timezone, boolean DST) {
+        this.AirportID = AirportID;
+        this.City = City;
+        this.Country = Country;
+        this.IATA_FAA = ITATA_FAA;
+        this.ICAO = ICAO;
+        this.Timezone = Timezone;
+        this.DST = DST;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public void getRunway() {
+        throw new UnsupportedOperationException();
+    }
+
+    public int getAirportID() {
+        return AirportID;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public int getIATA_FAA() {
+        return IATA_FAA;
+    }
+
+    public int getICAO() {
+        return ICAO;
+    }
+
+    public String getTimezone() {
+        return Timezone;
+    }
+
+    public boolean getDST() {
+        return DST;
+    }
 }
