b5e636422ab9afdd6a84d5899d0978cd1aa6c091
joh739 <joh739@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-28 22:23:26 +0000

    Added layers options to the options dialog. And added a graticule layer and a airway shape for the CTA. Made some crude and incorrect ways to access the acc and atc for mainly testing purposes.

--- Air Traffic Control/src/atc/gui/jfSettings.java
+++ Air Traffic Control/src/atc/gui/jfSettings.java
@@ -11,20 +11,122 @@
 package atc.gui;
 
 import SysBar.UnityItem;
+import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.layers.Layer;
+import gov.nasa.worldwindx.examples.LayerPanel;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
+import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.TitledBorder;
 
 /**
  *
  * @author Acer
  */
 public class jfSettings extends javax.swing.JDialog {
+
+    private WorldWindow wwd;
+    protected JPanel layersPanel;
+     protected Font defaultFont;
+     protected JPanel westPanel;
+    protected JScrollPane scrollPane;
+
+    public void setWwd(WorldWindow wwd) {
+        this.wwd = wwd;
+        makePanel(wwd);
+    }
+
     /** Creates new form jfSettings */
     public jfSettings(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+
+        if (parent != null) {
+            Dimension parentSize = parent.getSize();
+            Point p = parent.getLocation();
+            setLocation(p.x + parentSize.width / 4, p.y + parentSize.height / 4);
+        }
     }
     
+    protected void makePanel(WorldWindow wwd)
+    {
+        // Make and fill the panel holding the layer titles.
+        this.layersPanel = new JPanel(new GridLayout(0, 1, 0, 4));
+        this.layersPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        this.fill(wwd);
+
+        // Must put the layer grid in a container to prevent scroll panel from stretching their vertical spacing.
+        JPanel dummyPanel = new JPanel(new BorderLayout());
+        dummyPanel.add(this.layersPanel, BorderLayout.NORTH);
+
+        // Put the name panel in a scroll bar.
+        this.scrollPane = new JScrollPane(dummyPanel);
+        this.scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        // Add the scroll bar and name panel to a titled panel that will resize with the main window.
+        westPanel = new JPanel(new GridLayout(0, 1, 0, 10));
+        westPanel.setToolTipText("Layers to Show");
+        westPanel.add(scrollPane);
+        jPanelLayers.setLayout(new BorderLayout());
+        jPanelLayers.add(westPanel, BorderLayout.CENTER);
+    }
+    
+    public void fill(WorldWindow wwd) {
+        
+        // Fill the layers panel with the titles of all layers in the world window's current model.
+        for (Layer layer : wwd.getModel().getLayers())
+        {
+            LayerAction action = new LayerAction(layer, wwd, layer.isEnabled());
+            JCheckBox jcb = new JCheckBox(action);
+            jcb.setSelected(action.selected);
+            this.layersPanel.add(jcb);
+
+            if (defaultFont == null)
+            {
+                this.defaultFont = jcb.getFont();
+            }
+        }
+    }
+    
+    protected static class LayerAction extends AbstractAction
+    {
+        WorldWindow wwd;
+        private Layer layer;
+        private boolean selected;
+
+        public LayerAction(Layer layer, WorldWindow wwd, boolean selected)
+        {
+            super(layer.getName());
+            this.wwd = wwd;
+            this.layer = layer;
+            this.selected = selected;
+            this.layer.setEnabled(this.selected);
+        }
+
+        public void actionPerformed(ActionEvent actionEvent)
+        {
+            // Simply enable or disable the layer based on its toggle button.
+            if (((JCheckBox) actionEvent.getSource()).isSelected())
+                this.layer.setEnabled(true);
+            else
+                this.layer.setEnabled(false);
+
+            wwd.redraw();
+        }
+    }
+
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -36,8 +138,14 @@ public class jfSettings extends javax.swing.JDialog {
 
         btnAccept = new javax.swing.JButton();
         btnCancel = new javax.swing.JButton();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanelLayers = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Settings");
 
         btnAccept.setText("Accept");
         btnAccept.addActionListener(new java.awt.event.ActionListener() {
@@ -53,21 +161,78 @@ public class jfSettings extends javax.swing.JDialog {
             }
         });
 
+        javax.swing.GroupLayout jPanelLayersLayout = new javax.swing.GroupLayout(jPanelLayers);
+        jPanelLayers.setLayout(jPanelLayersLayout);
+        jPanelLayersLayout.setHorizontalGroup(
+            jPanelLayersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 539, Short.MAX_VALUE)
+        );
+        jPanelLayersLayout.setVerticalGroup(
+            jPanelLayersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 467, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Layers", jPanelLayers);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 539, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 467, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Sound", jPanel1);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 539, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 467, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Network", jPanel2);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 539, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 467, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Video", jPanel3);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(210, Short.MAX_VALUE)
-                .addComponent(btnCancel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnAccept)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnCancel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnAccept))
+                    .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 547, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(254, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 505, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAccept)
                     .addComponent(btnCancel))
@@ -117,7 +282,7 @@ public class jfSettings extends javax.swing.JDialog {
         //</editor-fold>
 
         /* Create and display the form */
-                java.awt.EventQueue.invokeLater(new Runnable() {
+        java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
                 jfSettings dialog = new jfSettings(new javax.swing.JFrame(), true);
@@ -135,5 +300,10 @@ public class jfSettings extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAccept;
     private javax.swing.JButton btnCancel;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanelLayers;
+    private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
 }
