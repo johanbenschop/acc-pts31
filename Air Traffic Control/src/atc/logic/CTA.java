e2dad48626105456d8226cfb545674b1ec8434dc
henk232 <henk232@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-10-31 11:11:21 +0000

    Collision detection finished... i hope!

--- Air Traffic Control/src/atc/logic/CTA.java
+++ Air Traffic Control/src/atc/logic/CTA.java
@@ -13,7 +13,7 @@ public class CTA {
     private Airport airport;
     private ArrayList<Airplane> airplaneList;
     private ArrayList<Airport> airportList;
-    
+
     public CTA(GeoLocation location, double width, double length) {
         this.location = location;
         this.width = width;
@@ -23,7 +23,7 @@ public class CTA {
         airportList = new ArrayList<>();
 
         try {
-           loadAirportList();
+            loadAirportList();
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         } catch (IOException ex) {
@@ -71,8 +71,8 @@ public class CTA {
      */
     public void Collision() {
         double lat1 = 0, lat2 = 0, lat3 = 0, lon1 = 0, lon2 = 0, lon3 = 0, bearing1 = 0, bearing2 = 0, bearing13 = 0, bearing12 = 0, bearing21 = 0, bearing23 = 0;
-        double dLat = 0, dLon = 0, dLon13 = 0, bearingA =0, bearingB = 0, dist12 = 0, dist13 = 0, alpha1 = 0, alpha2 = 0, alpha3 = 0, distance1 = 0, distance2 = 0, tijd1 = 0, tijd2 = 0;
-        
+        double dLat = 0, dLon = 0, dLon13 = 0, bearingA = 0, bearingB = 0, dist12 = 0, dist13 = 0, alpha1 = 0, alpha2 = 0, alpha3 = 0, distance1 = 0, distance2 = 0, time1 = 0, time2 = 0;
+
         if (airplaneList.size() > 1) {
             for (Airplane target : airplaneList) {
                 if (target.getStatus().equals(Airplane.Statusses.INFLIGHT)) {
@@ -85,19 +85,19 @@ public class CTA {
                                 lat2 = Math.toRadians(crashobject.getLocation().getLatitude());
                                 lon2 = Math.toRadians(crashobject.getLocation().getLongitude());
                                 bearing2 = Math.toRadians(crashobject.getDirection());
-                                
+
                                 bearing13 = bearing1;
-                                
+
                                 bearing23 = bearing2;
-                                
+
                                 dLat = lat2 - lat1;
                                 dLon = lon2 - lon1;
-                                dist12 = 2 * Math.asin(Math.sqrt(Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon /2)));
+                                dist12 = 2 * Math.asin(Math.sqrt(Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2)));
                                 bearingA = Math.acos((Math.sin(lat2) - Math.sin(lat1) * Math.cos(dist12))
                                         / (Math.sin(dist12) * Math.cos(lat1)));
                                 bearingB = Math.acos((Math.sin(lat1) - Math.sin(lat2) * Math.cos(dist12))
                                         / (Math.sin(dist12) * Math.cos(lat2)));
-                                
+
                                 if (Math.sin(lon2 - lon1) > 0) {
                                     bearing12 = bearingA;
                                     bearing21 = 2 * Math.PI - bearingB;
@@ -105,13 +105,12 @@ public class CTA {
                                     bearing12 = 2 * Math.PI - bearingA;
                                     bearing21 = bearingB;
                                 }
-                                
+
                                 alpha1 = (bearing13 - bearing12 + Math.PI) % (2 * Math.PI) - Math.PI;
                                 alpha2 = (bearing21 - bearing23 + Math.PI) % (2 * Math.PI) - Math.PI;
                                 if (Math.sin(alpha1) == 0 && Math.sin(alpha2) == 0) {
-                                    
                                 }
-                                
+
                                 alpha3 = Math.acos(-Math.cos(alpha1) * Math.cos(alpha2)
                                         + Math.sin(alpha1) * Math.sin(alpha2) * Math.cos(dist12));
                                 dist13 = Math.atan2(Math.sin(dist12) * Math.sin(alpha1) * Math.sin(alpha2),
@@ -122,14 +121,60 @@ public class CTA {
                                         Math.cos(dist13) - Math.sin(lat1) * Math.sin(lat3));
                                 lon3 = lon1 + dLon13;
                                 lon3 = (lon3 + 3 * Math.PI) % (2 * Math.PI) - Math.PI;
-                                
-                                
+
+                                distance1 = distFrom(lat1, lon1, lat3, lon3);
+                                time1 = distance1 / ((double) target.getSpeed() / 3600);
+                                distance2 = distFrom(lat2, lon2, lat3, lon3);
+                                time2 = distance1 / ((double) crashobject.getSpeed() / 3600);
+
+                                if (time1 >= time2) {
+                                    if (time1 - time2 <= 3 / ((double) crashobject.getSpeed() / 3600) && time1 - time2 >= -3000 / ((double) crashobject.getSpeed() / 3.6)) {
+                                        if (time1 - time2 <= 1 / ((double) crashobject.getSpeed() / 3.6)) {
+                                            target.setStatus(Airplane.Statusses.CRASHING1);
+                                            crashobject.setStatus(Airplane.Statusses.CRASHING1);
+                                        } else {
+                                            target.setStatus(Airplane.Statusses.CRASHING2);
+                                            target.setStatus(Airplane.Statusses.CRASHING1);
+                                        }
+                                    } else {
+                                        target.setStatus(Airplane.Statusses.INFLIGHT);
+                                        crashobject.setStatus(Airplane.Statusses.INFLIGHT);
+                                    }
+                                } else if (time1 <= time2) {
+                                    if (time1 - time2 <= 3 / ((double) target.getSpeed() / 3.6) && time1 - time2 >= -3000 / ((double) target.getSpeed() / 3.6)) {
+                                        if (time1 - time2 <= 1 / ((double) crashobject.getSpeed() / 3.6)) {
+                                            target.setStatus(Airplane.Statusses.CRASHING1);
+                                            crashobject.setStatus(Airplane.Statusses.CRASHING1);
+                                        } else {
+                                            target.setStatus(Airplane.Statusses.CRASHING2);
+                                            target.setStatus(Airplane.Statusses.CRASHING1);
+                                        }
+                                    }
+                                } else if (time1 == 0 && time2 == 0) {
+                                    target.setStatus(Airplane.Statusses.CRASHED);
+                                    crashobject.setStatus(Airplane.Statusses.CRASHED);
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    public double distFrom(double lat1, double lon1, double lat2, double lon2) {
+        double earthRadius = 3958.75;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLng = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double dist = earthRadius * c;
+
+        int meterConversion = 1609;
+
+        return new Double(dist * meterConversion).doubleValue();
     }
 
     /**
@@ -154,8 +199,7 @@ public class CTA {
      */
     public void deleteAirplane(int AirplaneNumber) {
         for (Airplane a : airplaneList) {
-            if(a.getAirplaneNumber() == AirplaneNumber)
-            {
+            if (a.getAirplaneNumber() == AirplaneNumber) {
                 airplaneList.remove(a);
             }
         }
@@ -164,16 +208,15 @@ public class CTA {
     //Loads airports from the airports.dat file
     //Todo : Deleting the "" in all strings gained from the aiport.dat file
     public void loadAirportList() throws FileNotFoundException, IOException {
-            FileInputStream fstream = new FileInputStream("airports.dat");
+        FileInputStream fstream = new FileInputStream("airports.dat");
 
-            DataInputStream in = new DataInputStream(fstream);
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-            String strline;
-            while((strline = br.readLine()) != null)
-            {
-             try {
-            String[] props = strline.split(",");
+        String strline;
+        while ((strline = br.readLine()) != null) {
+            try {
+                String[] props = strline.split(",");
                 int id = Integer.parseInt(props[0]);
                 String name = props[1];
                 String city = props[2];
@@ -185,9 +228,9 @@ public class CTA {
                 int altitude = Integer.parseInt(props[8]);
                 double timezone = Double.parseDouble(props[9]);
                 String dst = props[10];
-                
+
                 GeoLocation location = new GeoLocation(longitude, latitude, altitude);
-                
+
                 Airport airport = new Airport(id, name, city, country, iata_faa, icao, location, altitude, timezone, dst);
                 airportList.add(airport);
                 }  catch (NumberFormatException | InputMismatchException e) {
@@ -195,35 +238,28 @@ public class CTA {
                 }}
     }
 
- 
-
-    
     //Getters
     public Airplane getCurrentSelectedAirplane() {
         return airplane;
     }
-    
-    public ArrayList<Airplane> getAirplaneList()
-    {
+
+    public ArrayList<Airplane> getAirplaneList() {
         return airplaneList;
     }
-    
-    public ArrayList<Airport> getAirportList()
-    {
+
+    public ArrayList<Airport> getAirportList() {
         return airportList;
     }
-    
-    public Airplane getAirplane()
-    {
+
+    public Airplane getAirplane() {
         return airplane;
     }
-    
+
     public ListIterator<Airport> GetAirports() {
         return airportList.listIterator();
     }
-    
-    public Airport getAirport()
-    {
+
+    public Airport getAirport() {
         return airport;
     }
 }
