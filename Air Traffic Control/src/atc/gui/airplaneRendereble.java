1be19c6eb2a4c55c89acf270ab9c085e476f91cb
teysz007 <teysz007@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-11-08 13:40:09 +0000

    Altitude edited so an airplane could be seen flying (above the ground).

--- Air Traffic Control/src/atc/gui/airplaneRendereble.java
+++ Air Traffic Control/src/atc/gui/airplaneRendereble.java
@@ -32,7 +32,7 @@ public class airplaneRendereble extends Box {
         //super(flightplan.getAirplane().getLocation().toPosition(), 50000, 50000, 50000);
         this.flightplan = flightplan;
         this.airplane = flightplan.getAirplane();
-        //this.airplane.SetAimedAltitude(200);
+        this.airplane.SetAimedAltitude(200);
         
         this.timerAirplane = new Timer(500, new ActionListener() {
 
