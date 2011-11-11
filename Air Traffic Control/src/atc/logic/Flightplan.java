402d7e57c8759bd31c32ba1a49578d474a3be100
skillcoil <skillcoil@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-11-11 07:50:20 +0000

    Comments javadoc DONE

--- Air Traffic Control/src/atc/logic/Flightplan.java
+++ Air Traffic Control/src/atc/logic/Flightplan.java
@@ -1,16 +1,55 @@
 package atc.logic;
 
 import java.util.*;
-
+   
+     /**
+     * All information about a planned flight is found in Flightplan.
+     * 
+     */
 public class Flightplan {
 
+    /**************Datafields***********/
+    
+       
+     /**
+     * The Airport where the flight is going to. 
+     */
     private Airport destinationAirport;
+    
+    /**
+     * The Airport where the flight is taking off from
+     */
     private Airport takeoffAirport;
+    
+    /**
+     * Flightnumber of the flight.
+     */
     private int Flightnumber;
+    
+    /**
+     * The date and time when the flight departs
+     */
     private GregorianCalendar DepartureDate;
+    
+    /**
+     * The date and time of when the flight arrives.
+     */
     private GregorianCalendar ArrivalDate;
+    
+    /**
+     * The airplane that is gonna be used for the flight.
+     */
     private Airplane airplane;
     
+       /**A flightplan is made with the following parameters:
+     * @param destinationAirport: The Airport where the flight is going to. 
+     * @param takeoffAirport: The Airport where the flight is taking off from.
+     * @param flightnumber: Flightnumber of the flight.
+     * @param DepartureDate: The date and time when the flight departs.
+     * @param ArrivalDate: The date and time of when the flight arrives.
+     * @param airplane: The airplane that is gonna be used for the flight.
+     */
+  
     public Flightplan(Airport destination, Airport takeOff, int Flightnumber, GregorianCalendar departure, GregorianCalendar arrival, Airplane airplane) {
         destinationAirport = destination;
         takeoffAirport = takeOff;
@@ -19,7 +58,8 @@ public class Flightplan {
         ArrivalDate = arrival;
         this.airplane = airplane;
     }
-
+    /**************Getters**************/
+    
     public GregorianCalendar getArrivalDate() {
         return ArrivalDate;
     }
--- Air Traffic Control/src/atc/logic/GeoLocation.java
+++ Air Traffic Control/src/atc/logic/GeoLocation.java
@@ -4,18 +4,45 @@ import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.LatLon;
 import gov.nasa.worldwind.geom.Position;
 
+/**
+ * The coordinates on WorldWind of an object
+ * 
+ * 
+ */
 public class GeoLocation {
 
+    
+/**************Datafields***********/
+    
+    /**
+     * The Longitude of Geolocation.
+     */
     private double Longitude;
+    
+     /**
+     * The Latitude of Geolocation.
+     */
     private double Latitude;
+    
+     /**
+     * The Altitude of Geolocation.
+     */
     private double Altitude;
 
+    
+        /**A GeoLocation is made with the following parameters:
+     * @param Longitude: The longitude of GeoLocation.
+     * @param Latitude: The latitude of GeoLocation.
+     * @param Altitude: The altitude of GeoLocation.
+     */
     public GeoLocation(double Longitude, double Latitude, double Altitude) {
         this.Longitude = Longitude;
         this.Latitude = Latitude;
         this.Altitude = Altitude;
     }
-
+    
+    /**************Getters**************/
+    
     public double getAltitude() {
         return Altitude;
     }
@@ -29,6 +56,7 @@ public class GeoLocation {
     }
 
     
+    /**************Setters**************/
     
     public void setAltitude(double Altitude) {
         this.Altitude = Altitude;
@@ -45,15 +73,20 @@ public class GeoLocation {
     
 
     /**
+     * Method to return a string value with the longitude, latitude and altitude.
      * 
-     * @return 
+     * @return A string value with the longitude, latitude and altitude is returned.
      */
     public String ToString() {
         String gegevens;
         gegevens = "Longitude: " + Longitude + " , Latitude: " + Latitude + " , Altitude" + Altitude;
         return gegevens;
     }
-    
+     /**
+     * Method to return the Latlon made from the latitude and longitude
+     * 
+     * @return A LatLon value in degrees.
+     */
     public LatLon toLatLon() {
         return LatLon.fromDegrees(Latitude, Longitude);
     }
@@ -62,7 +95,11 @@ public class GeoLocation {
         return Position.fromDegrees(Latitude, Longitude, (double) Altitude);
     }
     
-    
+     /**
+     * Method to return a double value of the direction calculated between Airport a and Airport b.
+     * 
+     * @return A double value direction of Airport B from Airport A.
+     */
     public static double CalcDirection(Airport a, Airport b){
         GeoLocation locationA = new GeoLocation(0,0,0);
         GeoLocation locationB = new GeoLocation(0,0,0);
@@ -79,6 +116,12 @@ public class GeoLocation {
         return direction;
     }
     
+    
+     /**
+     * Method to return a double value of the direction calculated between GeoLocation a and GeoLocation b.
+     * 
+     * @return A double value direction of GeoLocation B from GeoLocation A.
+     */
         public static double CalcDirection(GeoLocation locationA, GeoLocation locationB) {
         double dLat = Math.toRadians(locationB.getLatitude() - locationA.getLatitude());
         double dLon = Math.toRadians(locationB.getLongitude() - locationA.getLongitude());
