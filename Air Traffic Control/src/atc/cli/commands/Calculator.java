fac54063ccce783ee6475c70556700bc60ffc604
joh739 <joh739@gmail.com>
2011-11-13 21:47:44 +0000

    Added begin of new type of Interface, commandline. Primary use is for fast testing and demoing and perhaps connecting to server.

---
+++ Air Traffic Control/src/atc/cli/commands/Calculator.java
@@ -0,0 +1,24 @@
+package atc.cli.commands;
+
+public class Calculator {
+
+    static public double add(double a, double b) {
+        return a + b;
+    }
+
+    static public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    static public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    static public double divide(double a, double b) {
+        return a / b;
+    }
+
+    static public double add(double a, double b, double c) {
+        return a + b + c;
+    }
+}
