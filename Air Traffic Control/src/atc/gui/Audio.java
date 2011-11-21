8119b5744bbba29ff37636a78e4ab1607ddfbdca
skillcoil <skillcoil@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-11-21 15:25:18 +0000

    Sound class complete, just need to be implemented in code.

--- Air Traffic Control/src/atc/gui/Audio.java
+++ Air Traffic Control/src/atc/gui/Audio.java
@@ -4,15 +4,56 @@
  */
 package atc.gui;
 
-import java.applet.Applet;
+import sun.audio.*;    //import the sun.audio package
+import java.io.*;
+
+/**
+ *
+ * @author Robbert
+ */
+public class Audio {
+
+    static String fileName;
+
+    static enum Statusses {
+
+        ALARM1, ALARM2, ALARM3, ALARM4, ALARM5,
+        ALARM6;
+    }
+
+    public static void play(Statusses s) throws FileNotFoundException, IOException {
+
+        switch (s) {
+            case ALARM1:
+                fileName = "src/atc/gui/resources/Alarm-1.wav";
+                break;
+            case ALARM2:
+                fileName = "src/atc/gui/resources/Alarm-2.wav";
+                break;
+            case ALARM3:
+                fileName = "src/atc/gui/resources/Alarm-3.wav";
+                break;
+            case ALARM4:
+                fileName = "src/atc/gui/resources/Alarm-4.wav";
+                break;
+            case ALARM5:
+                fileName = "src/atc/gui/resources/Alarm-5.wav";
+                break;
+            case ALARM6:
+                fileName = "src/atc/gui/resources/Alarm-6.wav";
+                break;
+        }
+        InputStream in = new FileInputStream(fileName);
+        AudioStream as = new AudioStream(in);
+        AudioPlayer.player.start(as);
+    }
+
+}
+/*import java.applet.Applet;
 import java.applet.AudioClip;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- *
- * @author johan
- */
 public class Audio {
     private AudioClip alarm;
     
@@ -26,3 +67,4 @@ public class Audio {
         }
     }
 }
+*/
