e1fe6bc073d7164a29269e2cfcb23f8114204f8c
jneek <jneek@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-12-05 09:27:28 +0000

    AirspaceTest klasse toegevoegd

---
+++ Air Traffic Control/test/atc/logic/AirspaceTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atc.logic;
+
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JNeek
+ */
+public class AirspaceTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
