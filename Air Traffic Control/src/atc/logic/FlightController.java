b17db0b1962e0f095ea6dbaeaede59e806a399d6
jneek <jneek@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-12-12 08:42:43 +0000

    Comments aangepast

--- Air Traffic Control/src/atc/logic/FlightController.java
+++ Air Traffic Control/src/atc/logic/FlightController.java
@@ -8,31 +8,57 @@ import java.util.Iterator;
  * @author Johan Benschop & Jeanique van der Sanden
  */
 public class FlightController {
+
+    /**************Datafields***********/
+    /**
+     * The id of the flightcontroller 
+     */
     private int ID;
+    /**
+     * The last ID that was given to a flightcontroller (+1 = next free ID) 
+     */
     private static int lastID;
+    /**
+     * Arraylist of lightplans assigned to the flightcontroller
+     */
     private ArrayList<Flightplan> flights;
-    
+
+    /***************Constructor**********/
+    /**
+     * This is a constructor used for making a Flightcontroller it assigns it
+     * an ID and creates a new Arraylist to contain flightplans
+     * 
+     */
     public FlightController() {
         ID = lastID++;
         flights = new ArrayList<>();
     }
-    
-    public int controllingFlights() {
-        return flights.size();
-    }
-    
+
+    /**
+     * Method to assign a flightplan to this Flightcontroller
+     * 
+     */
     public void assignFlight(Flightplan flightplan) {
         if (flightplan.getAssignedController() == null || flightplan.getAssignedController() != this) {
             flights.add(flightplan);
             flightplan.setAssignedController(this);
         }
     }
-    
+
+    /**
+     * Method to unassign a flightplan from this Flightcontroller
+     * 
+     */
     public void unassignFlight(Flightplan flightplan) {
         flights.remove(flightplan);
         flightplan.setAssignedController(null);
     }
-    
+
+    /**************Getters**************/
+    public int controllingFlights() {
+        return flights.size();
+    }
+
     public int getNumberAssignedFlights() {
         return flights.size();
     }
