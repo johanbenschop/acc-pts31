8f712a770749d9484a08caa81e2572ac44867f95
jneek <jneek@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2011-12-13 13:43:22 +0000

    Unittests aangepast/gemaakt
    2 ontbreken er nog en er zijn misschien een paar overbodige (maar dan moeten deze methodes nog uit CTA )
    De ontbrekende zijn CreateGreaterSector en distFrom, dit omdat het me gewoon duizeld met wiskundige berekeningen XD.

--- Air Traffic Control/test/atc/logic/CTATest.java
+++ Air Traffic Control/test/atc/logic/CTATest.java
@@ -20,15 +20,15 @@ import static org.junit.Assert.*;
  * @author johan
  */
 public class CTATest {
-    
+
     private CTA cta;
-    private Airspace airspace = new Airspace(); 
+    private Airspace airspace = new Airspace();
     private GeoLocation geoLocation, geoLocation2;
     private GeoSector geosector;
     private Airport airport;
     private Airplane airplane;
     private ArrayList<Airport> airportList = new ArrayList<Airport>();
-    
+
     public CTATest() {
     }
 
@@ -39,66 +39,90 @@ public class CTATest {
     @AfterClass
     public static void tearDownClass() throws Exception {
     }
-    
+
     @Before
     public void setUp() {
-        geoLocation = new GeoLocation(100,100,0); //Hier moeten nog goede waardes inkomen maar ik weet niet welke.
+        geoLocation = new GeoLocation(100, 100, 0);
         geosector = new GeoSector(0, 0, 0, 0);
-        cta = new CTA(geosector, airportList); // Hier moeten ook nog goede waardes in
-        geoLocation2 = new GeoLocation(2,2,2);
-        airport = new Airport(5555, "", "", "","", "", geoLocation, 0, 0, "");
-        airplane = new Airplane(600, 400, 10000, "", "", 10, 10, 10, 10000, 500, 0, 0, 10000, 0 , geoLocation, geoLocation2, 9999);
+        cta = new CTA(geosector, airportList);
+        geoLocation2 = new GeoLocation(2, 2, 2);
+        airport = new Airport(5555, "", "", "", "", "", geoLocation, 0, 0, "");
+        airplane = new Airplane(600, 400, 10000, "", "", 10, 10, 10, 10000, 500, 0, 0, 10000, 0, geoLocation, geoLocation2, 9999);
     }
-    
+
     @After
     public void tearDown() {
     }
     
-    /**
-     * Test of DetectAirplane method, of class CTA.
+    /** 
+     * Test of GetActiveAirplane method, of class CTA.
      */
     @Test
-    public void testDetectAirplane() {
-        System.out.println("DetectAirplane");
+    public void testGetActiveAirplane() {
+        System.out.println("GetActiveAirplane");
+        cta.addAirplane(airplane);
+        cta.GetActiveAirplane(9999);
+        Assert.assertEquals("Active Airplane has been set.", airplane, cta.getCurrentSelectedAirplane());
+    }
+
+    /** 
+     * Test of distFrom method, of class CTA.
+     */
+    @Test
+    public void testDistFrom() {
+        System.out.println("distFrom");
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
 
- 
-    /** 
+    /**                           TODO in deze methode wordt vliegtuig ook aan de collisionlist toegevoegd, dit moet nog worden getest!!
      * Test of addAirplane method, of class CTA.
      */
     @Test
     public void testAddAirplane() {
         System.out.println("addAirplane");
-         cta.addAirplane(airplane);
-         Assert.assertEquals("Airplane has been added.", airplane, cta.GetAirplane(9999));
-    }
-    
+        cta.addAirplane(airplane);
+        Assert.assertEquals("Airplane has been added.", airplane, cta.GetAirplane(9999));
 
-    
+    }
+
     /**
      * Test of deleteAirplane method, of class CTA.
      */
     @Test
     public void testDeleteAirplane() {
-        cta.removeAirplane(airplane);
-        Assert.assertEquals("Airplane should not be found in the list", null, cta.GetAirplane(9999));
         System.out.println("deleteAirplane");
-        //cta.deleteAirplane(airplane.getAirplaneNumber());
-        //Assert.assertEquals("Airport has been added.", false, cta.getAirportList().contains(airport));
+        cta.deleteAirplane(9999);
+        Assert.assertEquals("Airplane should not be found in the list", null, cta.GetAirplane(9999));
     }
-    
+
+    /** 
+     * Test of removeAirplane method, of class CTA.
+     */
+    @Test
+    public void testRemoveAirplane() {
+        System.out.println("removeAirplane");
+        cta.removeAirplane(airplane);
+        Assert.assertEquals("Airplane should not be found in the list", null, cta.GetAirplane((int) airplane.getId()));
+    }
+
     /**
      * Test of addAirport method, of class CTA.
      */
     @Test
     public void testAddAirport() {
-        cta.addAirport(airport);
-       Assert.assertEquals("Airplane has been added.", airport, cta.GetAirport(5555));
         System.out.println("addAirport");
-        //cta.addAirport(airport);
-        //Assert.assertEquals("Airport has been added.", true, cta.getAirportList().contains(airport));
+        cta.addAirport(airport);
+        Assert.assertEquals("Airplane has been added.", airport, cta.GetAirport(5555));
     }
-    
+
+    /** 
+     * Test of CreateGreaterSector method, of class CTA.
+     */
+    @Test
+    public void testCreateGreaterSector() {
+        System.out.println("CreateGreaterSector");
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 }
