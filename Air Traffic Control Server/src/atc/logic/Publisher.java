a7d0dd27f7784256a2c715102e612d3c4e070a9c
henk232 <henk232@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2012-01-15 14:56:16 +0000

    ticket 83: implemented the listener needed for using a push strategy still needs to be finalized when other things are done. 60 minutes of work.

---
+++ Air Traffic Control Server/src/atc/logic/Publisher.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atc.logic;
+
+import atc.interfaces.IAirplane;
+import atc.interfaces.RemoteListener;
+import atc.interfaces.RemotePublisher;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Henk
+ */
+public class Publisher extends UnicastRemoteObject implements RemotePublisher {
+    
+    private ArrayList<RemoteListener> listeners;
+    private final Object lockListener = new Object();
+    
+    public Publisher() throws RemoteException {
+        listeners = new ArrayList<>();
+    }
+    
+    public void publishAirplane(ArrayList<IAirplane> ap) throws RemoteException {
+        synchronized (lockListener) {
+            for (RemoteListener listener : listeners) {
+                listener.newAirplaneLocation(ap);
+            }
+        }
+    }
+    
+    public void publishFlightplan(Flightplan fp) throws RemoteException {
+        synchronized (lockListener) {
+            for (RemoteListener listener : listeners) {
+                listener.newFlightplan(fp);
+            }
+        }
+    }
+    
+    @Override
+    public void addListener(RemoteListener listener) throws RemoteException {
+        synchronized (lockListener) {
+            listeners.add(listener);
+            System.out.println("Listener added");
+        }
+    }
+
+    @Override
+    public void removeListener(RemoteListener listener) throws RemoteException {
+        synchronized (lockListener) {
+            listeners.remove(listener);
+            System.out.println("Listener removed");
+        }
+    }
+}
