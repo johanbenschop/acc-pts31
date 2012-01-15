6dcaefbfbc03f8cd03aec4040c43a9dcfbb0ba38
pokefan666 <pokefan666@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2012-01-15 21:58:03 +0000

    ticked 68. Logging werkt voor verandering in snelheid hoogte en richting. Het werkt ongeveer voor Crashing alleen heb ik ook hier een bug gevonden. zolang die niet gefixed is klopt de code voor logging nog niet precies.

--- Air Traffic Control/src/atc/logic/Logging.java
+++ Air Traffic Control/src/atc/logic/Logging.java
@@ -5,6 +5,7 @@
 package atc.logic;
 import com.sun.org.apache.xml.internal.serialize.LineSeparator;
 import java.io.*;
+import java.util.*;
 
 /**
  *
@@ -14,22 +15,35 @@ import java.io.*;
 public class Logging {
    private int AccID; 
    private int count = 0;
+   private String TextfileName;
    
    public Logging(int accID){
        AccID = accID;
+       TextfileName = AccID + ".txt";
     }
    
    public void WriteCommand(String Name, String Command)throws IOException{
        String name = Name;
        String command = Command;
-       String text = "\r" + AccID + " " + name + " " + command;
-       String TextfileName = AccID + ".txt";
+       String text = AccID + ": " + name + " " + command;
+       
        Writer output = null;
        FileOutputStream fStream = new FileOutputStream(TextfileName, true);
        DataOutputStream dStream = new DataOutputStream(fStream);
-       //File file = new File(TextfileName);
        output = new BufferedWriter(new OutputStreamWriter(dStream));
-       //output.write(text);
+       output.write(text);
+       output.write(java.security.AccessController.doPrivileged(new sun.security.action.GetPropertyAction("line.separator")));
+       output.close();
+       System.out.println("logged");
+   }
+   
+   public void WriteCollision(long airplane1, long airplane2, String name, String status)throws IOException{
+       Writer output = null;
+       String date = Calendar.MONTH + "/" + Calendar.DATE + "/" + Calendar.YEAR + " " + Calendar.HOUR + ":" + Calendar.MINUTE;
+       String text = AccID + ": " + airplane1 + " is in collision with " + airplane2 + ". status is " + status + ". Air traffic controller in control is: " + name + ". date: " + date;
+       FileOutputStream fStream = new FileOutputStream(TextfileName, true);
+       DataOutputStream dStream = new DataOutputStream(fStream);
+       output = new BufferedWriter(new OutputStreamWriter(dStream));
        output.write(text);
        output.write(java.security.AccessController.doPrivileged(new sun.security.action.GetPropertyAction("line.separator")));
        output.close();
