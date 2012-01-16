26f2787f03575c4788b2d0333cc3e34ed1f16ba4
pokefan666 <pokefan666@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2012-01-16 11:03:11 +0000

    ticked 68 logging opnieuw erin gezet vanuit iteratie 2.

--- Air Traffic Control Server/src/atc/logic/Logging.java
+++ Air Traffic Control Server/src/atc/logic/Logging.java
@@ -1,7 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package atc.logic;
-
 import com.sun.org.apache.xml.internal.serialize.LineSeparator;
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  *
@@ -10,23 +16,38 @@ import java.io.*;
 
 public class Logging {
    private int AccID; 
-   private int count = 0;
+   private String TextfileName;
    
    public Logging(int accID){
        AccID = accID;
+       TextfileName = AccID + ".txt";
     }
    
    public void WriteCommand(String Name, String Command)throws IOException{
        String name = Name;
        String command = Command;
-       String text = "\r" + AccID + " " + name + " " + command;
-       String TextfileName = AccID + ".txt";
+       String text = AccID + ": " + name + " " + command;
+       
        Writer output = null;
        FileOutputStream fStream = new FileOutputStream(TextfileName, true);
        DataOutputStream dStream = new DataOutputStream(fStream);
-       //File file = new File(TextfileName);
        output = new BufferedWriter(new OutputStreamWriter(dStream));
-       //output.write(text);
+       output.write(text);
+       output.write(java.security.AccessController.doPrivileged(new sun.security.action.GetPropertyAction("line.separator")));
+       output.close();
+       System.out.println("logged");
+   }
+   
+   public void WriteCollision(long airplane1, long airplane2, String name, String status)throws IOException{
+       Writer output = null;
+       DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+       //get current date time with Date()
+       Date date = new Date();
+       System.out.println(dateFormat.format(date));
+       String text = AccID + ": " + airplane1 + " is in collision with " + airplane2 + ". status is " + status + ". Air traffic controller in control is: " + name + ". date: " + dateFormat.format(date);
+       FileOutputStream fStream = new FileOutputStream(TextfileName, true);
+       DataOutputStream dStream = new DataOutputStream(fStream);
+       output = new BufferedWriter(new OutputStreamWriter(dStream));
        output.write(text);
        output.write(java.security.AccessController.doPrivileged(new sun.security.action.GetPropertyAction("line.separator")));
        output.close();
