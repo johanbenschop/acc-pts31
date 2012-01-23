05075533aec8c212839dcde0a0156faf5cef8aa7
henk232 <henk232@f47df53f-920d-4ceb-afb3-9b2318fd97f6>
2012-01-23 10:38:02 +0000

    Fixed publishers

--- Air Traffic Control Interface/src/atc/interfaces/RemotePublisher.java
+++ Air Traffic Control Interface/src/atc/interfaces/RemotePublisher.java
@@ -1,11 +1,14 @@
 package atc.interfaces;
 
 import java.rmi.*;
+import java.util.ArrayList;
 /**
  *
  * @author Henk, Johan & Mateusz
  */
 public interface RemotePublisher extends Remote {
+    public void publishFlightplans(ArrayList<IFlightplan> fp) throws RemoteException;
+    public void publishFlightplan(IFlightplan fp) throws RemoteException;
     public void addListener(RemoteListener listener) throws RemoteException;
     public void removeListener(RemoteListener listener) throws RemoteException;
 }
